f64a5357faca2a8724817ed97a1d93f6b60a0c07
Jared Nordstrom <nords316@d.umn.edu>
2023-03-24 14:55:32 -0500

    added skeleton

---
+++ lib/dreams/utils/page_skeleton.dart
@@ -0,0 +1,53 @@
+/*
+Add to main at the bottom [
+    class ScreenName extends StatefulWidget {
+      @override
+      _ScreenName createState() => _ScreenName();
+    }
+
+    class _ScreenName extends State<ScreenName> {
+      @override
+      Widget build(BuildContext context) {
+        return new PageName();
+      }
+    }
+]
+
+File Skeleton [
+    import 'package:flutter/cupertino.dart';
+    import 'package:flutter/material.dart';
+
+    class PageName extends StatefulWidget {
+
+      PageName();
+
+      @override
+      _PageNameState createState() => _PageNameState();
+    }
+
+    class _PageNameState extends State<PageName>  {
+      @override
+      void initState() {
+        super.initState();
+      }
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Title"),
+            centerTitle: true,
+            backgroundColor: Colors.blueAccent.shade700,
+          ),
+          backgroundColor: Colors.white,
+          body: Center(
+              child: Column(
+                children: <Widget>[
+                  //Insert the build within here
+                ],
+              )
+          ),
+        );
+      }
+    }
+]
+ */
