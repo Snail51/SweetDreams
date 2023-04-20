6abfe86d555bec2d663373b59869bf53f74e5826
Brendan Rood <rood0039@umn.edu>
2023-04-20 11:28:21 -0500

    Added many new sounds
    all sounds found from https://www.storyblocks.com/ and are royalty free with our university accounts

--- lib/dreams/views/sounds_page.dart
+++ lib/dreams/views/sounds_page.dart
@@ -19,7 +19,7 @@ class _SoundsPageState extends State<SoundsPage> {
   List<WidgetAudioPlayer> players = [];
   List<Widget> displayables = [];
 
-  DateTime loadEnd = DateTime.now().add(Duration(seconds: 10));
+  DateTime loadEnd = DateTime.now().add(Duration(seconds: 20));
   Timer refreshTimer = Timer.periodic(Duration(hours: 2), (timer) { });
   Timer loadingTimer = Timer(Duration(hours: 2), () { });
   Widget loadHolder = Text("NULL");
@@ -57,7 +57,7 @@ class _SoundsPageState extends State<SoundsPage> {
     if(loading && !widget.soundsLoaded)
       {
         setState(() {
-          loadingProgress = 1.0 - (DateTimeRange(start: DateTime.now(), end: loadEnd).duration.inSeconds.toDouble() / 10.0);
+          loadingProgress = 1.0 - (DateTimeRange(start: DateTime.now(), end: loadEnd).duration.inSeconds.toDouble() / 20.0);
           loadHolder = loader();
         });
       }
@@ -106,17 +106,46 @@ class _SoundsPageState extends State<SoundsPage> {
       },
     );
 
+    players.add(WidgetAudioPlayer("Morning Meadow", "meadow_morning.mp3", Icon(Icons.agriculture))); //SBA
+    players.add(WidgetAudioPlayer("Bird", "Bird.mp3", Icon(Icons.notification_important))); //DEX
+    players.add(WidgetAudioPlayer("Sparrows", "sparrows.mp3", Icon(Icons.forest))); //SBA
+    players.add(WidgetAudioPlayer("Dogs Barking", "dogs_barking.mp3", Icon(Icons.pets))); //SBA
 
-    players.add(WidgetAudioPlayer("Summer Night", "Summer_Night.mp3", Icon(Icons.wb_twighlight, color: Colors.white)));
-    players.add(WidgetAudioPlayer("Rain", "Rain.mp3", Icon(Icons.wb_cloudy, color: Colors.white)));
-    players.add(WidgetAudioPlayer("Wind", "Wind.mp3", Icon(Icons.wind_power, color: Colors.white)));
-    players.add(WidgetAudioPlayer("Womb", "Womb.mp3", Icon(Icons.favorite, color: Colors.white)));
-    players.add(WidgetAudioPlayer("Brook", "Brook.mp3", Icon(Icons.water, color: Colors.white)));
-    players.add(WidgetAudioPlayer("Ocean", "Ocean.mp3", Icon(Icons.tsunami, color: Colors.white)));
-    players.add(WidgetAudioPlayer("Bird", "Bird.mp3", Icon(Icons.notification_important, color: Colors.grey)));
-    players.add(WidgetAudioPlayer("Seagull", "Seagull.mp3", Icon(Icons.beach_access, color: Colors.grey)));
-    players.add(WidgetAudioPlayer("Bouy", "Bouy.mp3", Icon(Icons.directions_boat, color: Colors.grey)));
-    players.add(WidgetAudioPlayer("Thunder", "Thunder.mp3", Icon(Icons.bolt, color: Colors.grey)));
+    players.add(WidgetAudioPlayer("Summer Night", "Summer_Night.mp3", Icon(Icons.wb_twighlight))); //DEX
+    players.add(WidgetAudioPlayer("Leaves Crunching", "dry_leaves.mp3", Icon(Icons.park))); //SBA
+
+    players.add(WidgetAudioPlayer("Rain", "Rain.mp3", Icon(Icons.wb_cloudy))); //DEX
+    players.add(WidgetAudioPlayer("Rain on Window", "rain_on_glass.mp3", Icon(Icons.window))); //SBA
+    players.add(WidgetAudioPlayer("Thunder", "Thunder.mp3", Icon(Icons.bolt))); //DEX
+
+    players.add(WidgetAudioPlayer("Brook", "Brook.mp3", Icon(Icons.water))); //DEX
+
+    players.add(WidgetAudioPlayer("Ocean", "Ocean.mp3", Icon(Icons.tsunami))); //DEX
+    players.add(WidgetAudioPlayer("Seagull", "Seagull.mp3", Icon(Icons.beach_access))); //DEX
+    players.add(WidgetAudioPlayer("Bouy", "Bouy.mp3", Icon(Icons.directions_boat))); //DEX
+    players.add(WidgetAudioPlayer("Rowboat", "paddle_rowing.mp3", Icon(Icons.rowing))); //SBA
+
+    players.add(WidgetAudioPlayer("Industry", "industrial_transport.mp3", Icon(Icons.factory))); //SBA
+    players.add(WidgetAudioPlayer("Distant City", "distant_city.mp3", Icon(Icons.location_city))); //SBA
+    players.add(WidgetAudioPlayer("Powerplant", "power_generator.mp3", Icon(Icons.electric_bolt))); //SBA
+    players.add(WidgetAudioPlayer("Wind", "Wind.mp3", Icon(Icons.wind_power))); //DEX
+    players.add(WidgetAudioPlayer("Wind Farm", "wind_turbines.mp3", Icon(Icons.wind_power))); //SBA
+
+    players.add(WidgetAudioPlayer("Typing", "keyboard_typing.mp3", Icon(Icons.keyboard))); //SBA
+    players.add(WidgetAudioPlayer("Bullet Train", "train_fast.mp3", Icon(Icons.directions_train))); //SBA
+    players.add(WidgetAudioPlayer("Library", "books.mp3", Icon(Icons.book))); //SBA
+
+    players.add(WidgetAudioPlayer("Music Box", "music_box.mp3", Icon(Icons.bedroom_baby))); //SBA
+    players.add(WidgetAudioPlayer("Campanile Bells", "campanile_bells.mp3", Icon(Icons.notifications))); //SBA
+    players.add(WidgetAudioPlayer("Large Bell", "large_bell.mp3", Icon(Icons.circle_notifications))); //SBA
+    players.add(WidgetAudioPlayer("Church Bells", "church_bells.mp3", Icon(Icons.church))); //SBA
+
+    players.add(WidgetAudioPlayer("Brown Noise", "brown_noise.mp3", Icon(Icons.multitrack_audio))); //SBA
+    players.add(WidgetAudioPlayer("Pink Noise", "pink_noise.mp3", Icon(Icons.multitrack_audio))); //SBA
+    players.add(WidgetAudioPlayer("Rolling Ball", "ball_rolling.mp3", Icon(Icons.sports_basketball))); //SBA
+    players.add(WidgetAudioPlayer("Womb", "Womb.mp3", Icon(Icons.favorite))); //DEX
+
+
 
     update();
     super.initState();
