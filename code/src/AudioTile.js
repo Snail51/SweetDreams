345abfcf7e9b5795f7c394302a94b562f09507d6
mobren11 <mobren11@gmail.com>
2025-03-13 14:58:48 -0500

    pull audiotile logic into seperate file
    all logic around the construction of AudioTile elements from <AudioNode> moved into seperate js and css that are included at runtime

---
+++ code/src/AudioTile.js
@@ -0,0 +1,69 @@
+import { AudioNode } from "./AudioNode.js";
+window.audioCTX = new window.AudioContext();
+window.audioNodes = new Array();
+
+var audioNodeDOMs = document.querySelectorAll("AudioNode");
+for( var node of audioNodeDOMs )
+{
+    // capture DOM element attributes
+    const name = node.getAttribute("name");
+    const src = node.getAttribute("src");
+    const icon = node.getAttribute("icon");
+    const metaLeft = node.getAttribute("metaLeft");
+    const metaRight = node.getAttribute("metaRight");
+
+    // create new div object to hold the button grid and slider
+    var newDiv = document.createElement("div");
+    newDiv.className = "audioContainer";
+
+    // create new AudioNode object
+    var newNode = new AudioNode(src, window.audioCTX, newDiv);
+    window.audioNodes.push(newNode);
+    const nodeIndex = window.audioNodes.length-1;
+
+    // create new button object
+    var newButton = document.createElement("button");
+    newButton.className = "audioButton";
+    newButton.addEventListener("click", () => window.audioNodes[nodeIndex].toggle());
+
+    // create new <p> for LEFT metadata
+    var newMetaLeft = document.createElement("div");
+    newMetaLeft.className = "META-L";
+    newMetaLeft.innerHTML = metaLeft;
+
+
+    var newIcon = document.createElement("div");
+    newIcon.className = "ICON";
+    newIcon.innerHTML = `<i class="fa-solid ${icon}"></i>`
+
+    // create new <input> of type `range`
+    var newRange = document.createElement("input");
+    newRange.type = "range";
+    newRange.className = "SLIDER";
+    newRange.addEventListener("input", () => window.audioNodes[nodeIndex].adjustVolume(event.target.value/100));
+
+    // create new <p> for RIGHT metadata
+    var newMetaRight = document.createElement("div");
+    newMetaRight.className = "META-R";
+    newMetaRight.innerHTML = metaRight;
+
+    // create new <p> for LABEL
+    var newLabel = document.createElement("div");
+    newLabel.className = "LABEL";
+    newLabel.innerHTML = name;
+
+    // insert the elements into the div
+    newButton.appendChild(newMetaLeft);
+    newButton.appendChild(newIcon);
+    newButton.appendChild(newMetaRight);
+    newButton.appendChild(newLabel);
+
+    newDiv.appendChild(newButton);
+    newDiv.appendChild(newRange);
+
+    // place the new div structure onto the DOM
+    node.insertAdjacentElement("afterend", newDiv);
+
+    // remove placeholder node of type <AudioNode>
+    node.remove();
+}
