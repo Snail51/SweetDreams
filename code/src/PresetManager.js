ac3de88e95e9d4db8f29f093c45b2ae74c511b11
mobren11 <mobren11@gmail.com>
2025-05-11 09:28:06 -0500

    decrease significant figures for volume in preset manager
    when saving a preset, only save the volume level to the hundredths place. this reduces the amount of data that needs to be stored, allowing for smaller share links

--- code/src/PresetManager.js
+++ code/src/PresetManager.js
@@ -44,7 +44,7 @@ export class Saver
         {
             if(node.playing)
             {
-                this.activeList.push(node.src + ";" + node.volume);
+                this.activeList.push(node.src + ";" + ((Math.round(node.volume*100))/100));
             }
         }
         var payload = this.activeList.join(",");
