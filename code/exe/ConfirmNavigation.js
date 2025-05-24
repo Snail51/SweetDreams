fd4ef77a960f426b42aa85b4d3d2d810fa833104
mobren11 <mobren11@gmail.com>
2025-05-24 17:11:10 -0500

    fix confirm navigation
    we need to put the check in a sperate function to prevent js from optimising the answer to a static value

--- code/exe/ConfirmNavigation.js
+++ code/exe/ConfirmNavigation.js
@@ -1,5 +1,5 @@
 // MINIFIED VERSION OF THE FILE OF THE SAME NAME IN THE `src` FOLDER
 // MINIFIED WITH https://www.toptal.com/developers/javascript-minifier
-// MINIFIED AT Sat May 24 16:45:00 CDT 2025
+// MINIFIED AT Sat May 24 17:06:58 CDT 2025
 
-window.addEventListener("beforeunload",e=>{window.audioNodes&&window.audioNodes.some(e=>!0===e.playing)&&e.preventDefault()});
+window.shouldConfirmNavigation=function(){return!!(window.audioNodes&&window.audioNodes.some(o=>!0===o.playing))},window.addEventListener("beforeunload",o=>{window.shouldConfirmNavigation()&&o.preventDefault()});
--- code/src/ConfirmNavigation.js
+++ code/src/ConfirmNavigation.js
@@ -1,5 +1,17 @@
-window.addEventListener("beforeunload", (event) => {
+window.shouldConfirmNavigation = function()
+{
     if(window.audioNodes ? window.audioNodes.some(node => node.playing === true) : false)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+window.addEventListener("beforeunload", (event) => {
+    if(window.shouldConfirmNavigation())
     {
         event.preventDefault();
         return;
